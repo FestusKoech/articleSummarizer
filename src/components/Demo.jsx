8e624fbf5e201e1a59f9ead8fe1ce10a5d71bd9e
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-05-12 12:18:19 +0300

    Implemented RapidAPI call

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,38 +1,76 @@
- import {useState, useEffect} from 'react';
- import {copy, linkIcon, loader, tick} from '../assets';
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets';
+import useLazyGetSummaryQuery from '../services/article';
 
 const Demo = () => {
-const [article, setArticle] = useState({
- url: '',
- summary:'',
-});
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
 
-const handleSubmit = async (e) => {
-alert('Submitted');
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles')
+
+    )
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [
+
+  ]);
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+    }
+  }
+
+const handleCopy = (copyUrl) =>{
+setCopied(copyUrl);
+navigator.clipboard.writeText(copyUrl);
+setTimeout(() => setCopied(false), 3000);
 }
 
   return (
-    <div>
+  
       <section className="mt-16 w-full max-w-xl">
         {/* {Search} */}
         <div className="flex flex-col w-full gap-2">
           <form
-          className='relative flex justify-center items-center'
-          onSubmit= {handleSubmit}>
+            className='relative flex justify-center items-center'
+            onSubmit={handleSubmit}>
 
-            <img src={linkIcon} alt='linkIcon' 
-            className='absolute left-0 my-2 ml-3 w-5' />
+            <img src={linkIcon} alt='linkIcon'
+              className='absolute left-0 my-2 ml-3 w-5' />
 
             <input type='url'
-            placeholder='Enter a URL'
-            value= {article.url}
-            onChange={(e)=>setArticle({...article, url: e.target.value})}
-            required
-            className='url_input peer' />
+              placeholder='Enter a URL'
+              value={article.url}
+              onChange={(e) => setArticle({ ...article, url: e.target.value })}
+              required
+              className='url_input peer' />
 
             <button
-            type='submit'
-            className='submit_btn 
+              type='submit'
+              className='submit_btn 
             peer-focus:border-gray-500 
             peer-focus:text-gray-500'
             >
@@ -42,13 +80,70 @@ alert('Submitted');
           </form>
           {/* History */}
 
+          <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+            {
+              allArticles.map(
+                (item, index)=> (
+                  <div key={`link-${index}`}
+                  onClick={ () => setArticle(item)}
+                  className='link_card'>
+
+                    <div className="copy_btn"
+                    onClick={() =>(handleCopy(item.url))}
+                    >
+                      <img src={copied === item.url ? tick : copy} 
+                      alt='copy_icon' 
+                      className='w-[40%] h-[40%] object-contain'/>
+
+                    </div>
+                    <p className='flex-1 font-Teachers text-small truncate text-gray-700 font-medium text-sm'>
+                      {item.url}
+                    </p>
+
+                    </div>
+
+
+                )
+              )
+            }
+
+          </div>
+
 
         </div>
         {/* Display feedback */}
 
+        <div className="my-10 max-w-full flex justify-center items-center">
+
+          {isFetching ? (
+            <img src={loader} alt='loading' className='w-20 h-20 object-contain'/>
+          ):
+          error?(
+            <p className='font-Teachers font-bold text-black'>
+              Well... That is embarrasing. <br /> Try once more 
+              <span className='font-Teachers font-normal font-gray-700'>
+                {error?.data?.error}
+                </span>
+                </p>
+                ):
+                (article.summary && (
+                  <div className='flex flex-col gap-3'>
+                    <h2 className='font-Teachers font-bold text-black'>Article <span className='blue-gradient'>Summary</span> </h2>
+
+                    <div className='summary-box'>
+                      <p>{article.summary}</p>
+
+                    </div>
+                    </div>
+
+                )
+              )};
+
+        </div>
+
       </section>
-      
-    </div>
+
+    
   )
 }
 
--- src/services/article.js
+++ src/services/article.js
@@ -1,12 +1,28 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const RapidAPIKey = import.meta.env.VITE_RAPIDAPI_AISUMMARIZER_KEY;
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', RapidAPIKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+
+    }),
+
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `test`
+            query: (params) =>
+                `/summarize?url=${encodeURIComponent(params.articleUrl)}$length=3`
 
         })
 
     })
-})
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
